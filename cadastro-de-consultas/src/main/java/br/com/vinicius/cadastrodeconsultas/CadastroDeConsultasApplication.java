bd112add5e68f1384e7ebd3adb7b07c0456f67f0
Vinicius Canuto Falcão <viniciusfalcao82@gmail.com>
2022-11-16 17:09:03 -0300

    trazendo api

---
+++ cadastro-de-consultas/src/main/java/br/com/vinicius/cadastrodeconsultas/CadastroDeConsultasApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.cadastrodeconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeConsultasApplication.class, args);
+	}
+
+}
---
+++ cadastro-de-consultas/src/test/java/br/com/vinicius/cadastrodeconsultas/CadastroDeConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.cadastrodeconsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
